6ff307866a4a0f372dba94379ff94292913a01a7
Borys <borys.tkachenko@hootsuite.com>
2023-10-13 10:41:00 -0700

    Initial commit

---
+++ TestDocCProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestDocCProject
+//
+//  Created by Borys Tkachenko on 2023-10-13.
+//
+
+import SwiftUI
+
+/// Main view
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ TestDocCProject/TestDocCProjectApp.swift
@@ -0,0 +1,18 @@
+//
+//  TestDocCProjectApp.swift
+//  TestDocCProject
+//
+//  Created by Borys Tkachenko on 2023-10-13.
+//
+
+import SwiftUI
+
+@main
+/// Test documentation
+struct TestDocCProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
