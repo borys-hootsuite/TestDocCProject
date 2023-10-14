84482547328486489d25e1a21bad0941ce1adb11
Borys <borys.tkachenko@hootsuite.com>
2023-10-13 17:09:11 -0700

    Update script

--- build_docs.sh
+++ build_docs.sh
@@ -28,16 +28,24 @@ git config user.email "$DOCC_GITHUB_EMAIL"
 
 # Change the GitHub URL to your repository
 git remote set-url origin https://$DOCC_GITHUB_USERNAME:$DOCC_GITHUB_API_TOKEN@github.com/borys-hootsuite/TestDocCProject/
-git fetch
-git stash push -u  -- docs doc_archives
+# git fetch
+# git stash push -u  -- docs doc_archives
+
+CURRENT_BRANCH=$(git branch --show-current)
+git stash
+
 git checkout feature/docc-hosting
 
 # rm -rf docs doc_archives
 
-git stash apply
-git add docs doc_archives
+# git stash apply
+git add docs #doc_archives
 git commit -m "Updated DocC documentation"
 git push --set-upstream origin feature/docc-hosting
+
+# Switch back to current branch
+git checkout $CURRENT_BRANCH
+git stash pop
 
 rm -rf docsData
 rm -rf docs
